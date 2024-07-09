d2de5d2c87d47ad37efcd4fa26dd488bb995f907
wadallace <77767227+wadallace@users.noreply.github.com>
2024-07-09 11:03:21 -0400

    more setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,209 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id='root'>
+        <main className='content row'>
+          <h1>Retro Board</h1>
+
+          <div className='RetroApp row'>
+            <div className='RetroCategory RetroCategory-1'>
+              <h2>Went Well</h2>
+
+              <button
+                type='button'
+                className='ButtonAdd button button-default'
+                aria-label='Add new card'
+                title='Add new card'
+              >
+                +
+              </button>
+
+              <div
+                className='RetroCard'
+                aria-label='Retro card'
+              >
+                <textarea
+                  className='textbox'
+                  placeholder='Enter text here'
+                  aria-label='Enter text here'
+                  rows='1'
+                  value='Here is an example card'
+                ></textarea>
+
+                <div className='button-group'>
+                  <button
+                    type='button'
+                    className='button button-left'
+                    title='Move left'
+                  >
+                    ⟨
+                  </button>
+                  <button
+                    type='button'
+                    className='button button-delete'
+                    title='Delete'
+                  >
+                    ⓧ
+                  </button>
+                  <div>
+                    <button
+                      type='button'
+                      className='button button-left'
+                      title='Like'
+                    >
+                      👍 5
+                    </button>
+                    <button
+                      type='button'
+                      className='button button-left'
+                      title='Dislike'
+                    >
+                      👎 0
+                    </button>
+                    <button
+                      type='button'
+                      className='button button-right'
+                      title='Move right'
+                    >
+                      ⟩
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='RetroCategory RetroCategory-2'>
+              <h2>To Improve</h2>
+              <button
+                type='button'
+                className='ButtonAdd button button-default'
+                aria-label='Add to new card'
+                title='Add to new card'
+              >
+                +
+              </button>
+              <div
+                className='RetroCard'
+                aria-label='Retro card'
+              >
+                <textarea
+                  className='textbox'
+                  placeholder='Enter text here'
+                  aria-label='Enter text here'
+                  rows='1'
+                  value='Here is an example card'
+                ></textarea>
+
+                <div className='button-group'>
+                  <button
+                    type='button'
+                    className='button button-left'
+                    title='Move left'
+                  >
+                    ⟨
+                  </button>
+                  <button
+                    type='button'
+                    className='button button-delete'
+                    title='Delete'
+                  >
+                    ⓧ
+                  </button>
+                  <div>
+                    <button
+                      type='button'
+                      className='button button-left'
+                      title='Like'
+                    >
+                      👍 5
+                    </button>
+                    <button
+                      type='button'
+                      className='button button-left'
+                      title='Dislike'
+                    >
+                      👎 0
+                    </button>
+                    <button
+                      type='button'
+                      className='button button-right'
+                      title='Move right'
+                    >
+                      ⟩
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='RetroCategory RetroCategory-3'>
+              <h2>Action Items</h2>
+              <button
+                type='button'
+                className='ButtonAdd button button-default'
+                aria-label='Add to new card'
+                title='Add to new card'
+              >
+                +
+              </button>
+              <div
+                className='RetroCard'
+                aria-label='Retro card'
+              >
+                <textarea
+                  className='textbox'
+                  placeholder='Enter text here'
+                  aria-label='Enter text here'
+                  rows='1'
+                  value='Here is an example card'
+                ></textarea>
+
+                <div className='button-group'>
+                  <button
+                    type='button'
+                    className='button button-left'
+                    title='Move left'
+                  >
+                    ⟨
+                  </button>
+                  <button
+                    type='button'
+                    className='button button-delete'
+                    title='Delete'
+                  >
+                    ⓧ
+                  </button>
+                  <div>
+                    <button
+                      type='button'
+                      className='button button-left'
+                      title='Like'
+                    >
+                      👍 5
+                    </button>
+                    <button
+                      type='button'
+                      className='button button-left'
+                      title='Dislike'
+                    >
+                      👎 0
+                    </button>
+                    <button
+                      type='button'
+                      className='button button-right'
+                      title='Move right'
+                    >
+                      ⟩
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
